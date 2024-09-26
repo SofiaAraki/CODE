abd9a1d9209de9cd69fe051cc26366a2383b39b6
SofiaAraki <fernandasofia589@gmail.com>
2024-09-26 13:55:55 -0300

    imprementado "jogar novamente"

--- js/game.js
+++ js/game.js
@@ -1,7 +1,9 @@
+//
 const grid = document.querySelector('.grid');
 const spanPlayer = document.querySelector('.player');
 const timer = document.querySelector('.timer');
 
+// cria uma lista de cartas
 const characters = [
     'css',
     'html',
@@ -15,111 +17,132 @@ const characters = [
     'swift',
 ];
 
+//
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
     return element;
 }   
 
+//let só pode ser acessado dentro do escopo onde foi criado
+//define variaveis para verificar as cartas selecionadas
 let firstCard = '';
 let secondCard = '';
 
+//cria uma função para verificar se o jogo terminou
 const checkEndGame = () => {
 
     const disabledCards = document.querySelectorAll('.disabled-card');
-
+    // verifica se o número de cartas desabilitadas é igual ao número de cartas totais
+    // isso indica o fim do jogo, portanto para o tempo e exibe uma mensagem de parabéns
     if (disabledCards.length === 20) {
-
         clearInterval(this.loop);
         alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`);
-
+        window.confirm('Deseja jogar novamente?');
+        window.location.reload();
     }
 }
 
-
+//cria uma função para verificar se as cartas selecionadas são iguais
 const checkCards = () => {
 
+    // cria uma variável para verificar se as cartas selecionadas são iguais
     const firstCharacter = firstCard.getAttribute('data-character');
     const secondCharacter = secondCard.getAttribute('data-character');
 
+    // verifica se as cartas selecionadas são iguais
     if (firstCharacter === secondCharacter) {
         
+        // dasabilita as cartas selecionadas
         firstCard.firstChild.classList.add('disabled-card');
         secondCard.firstChild.classList.add('disabled-card');
 
         firstCard = '';
         secondCard = '';
-
+    
+    // caso as cartas selecionadas não sejam iguais, remove a classe 'reveal-card' das cartas selecionadas
+    // ou seja, vira elas novamente
     } else {
-
         setTimeout(() => {
-
             firstCard.classList.remove('reveal-card');
             secondCard.classList.remove('reveal-card');            
 
             firstCard = '';
             secondCard = '';
-
         }, 500);
-
     }
 }
 
-
+// cria uma função para revelar as cartas selecionadas
 const revealCard = ({ target }) => {
-
+    
+    // verifica se a carta selecionada já está revelada
     if (target.parentNode.className.includes('reveal-card')) {
       return;
     }
-  
+    // verifica se já foi selecionada uma carta
     if (firstCard === '') {
   
       target.parentNode.classList.add('reveal-card');
       firstCard = target.parentNode;
-  
+    
+    // verifica se já foi selecionada a segunda carta
     } else if (secondCard === '') {
   
       target.parentNode.classList.add('reveal-card');
       secondCard = target.parentNode;
-  
-      checkCards();
-  
+    // chama a função para verificar se as cartas selecionadas são iguais
+      checkCards(); 
     }
 }
 
+// cria uma função para criar as cartas
 const createCard = (character) => {
 
+    // 
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
   
     front.style.backgroundImage = `url('../images/${character}.png')`;
-  
+    
+    // adiciona um nó filho à carta
     card.appendChild(front);
     card.appendChild(back);
-  
+    
+    // adiciona um evento de clique à carta
     card.addEventListener('click', revealCard);
+
+    //
     card.setAttribute('data-character', character)
   
     return card;
 }
 
+// cria uma função para carregar o jogo
 const loadGame = () => {
+
+    // cria um array para duplicar as cartas
     const duplicateCharacters = [...characters, ...characters];
-  
+    
+    // cria um array ordenado aleatoriamente
+    // serve para distribuir as cartas aleatoriamente no tabuleiro
     const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
-  
+    
+    // 
     shuffledArray.forEach((character) => {
       const card = createCard(character);
       grid.appendChild(card);
     });
 }
 
+// contador de tempo
 const startTimer = () => {
 
     this.loop = setInterval(() => {
       const currentTime = +timer.innerHTML;
       timer.innerHTML = currentTime + 1;
+      checkEndGame();
     }, 1000);
   
 }
--- js/login.js
+++ js/login.js
@@ -1,10 +1,11 @@
+//cria uma função que recupera os valores
 const input = document.querySelector('.login_input');
 const button = document.querySelector('.login_button');
 const form = document.querySelector('.login-form');
 
-
+//cria uma função para habilitar o botão de 'Play' apos escrever o nome
 const validateInput = ({ target }) => {
-    if (target.value.length > 3) {
+    if (target.value.length >= 3) {
         button.removeAttribute('disabled');
         return;
     }
@@ -12,6 +13,7 @@ const validateInput = ({ target }) => {
     button.setAttribute('disabled', '');
 }
 
+//cria uma função para enviar o nome do jogador para o localStorage
 const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -19,5 +21,7 @@ const handleSubmit = (event) => {
     window.location = 'pages/game.html';
 }
 
+// chama a função para conferir o nome inserido
 input.addEventListener('input', validateInput);
+// chama a funçao para enviar o nome do jogador
 form.addEventListener('submit', handleSubmit);
